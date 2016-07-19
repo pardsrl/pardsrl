87612b9283ac913eaea39a3ed6292b69db45717e
Pablo Santiago Semhan <santiagosemhan@gmail.com>
2016-07-19 15:13:19 -0300

    Habilito CORS en express

--- rt/app.js
+++ rt/app.js
@@ -8,6 +8,13 @@ const TRMAN_SAI280  = '/srv/data/trman.sai280';
 const HSTPL_SAI280  = '/srv/data/hstpl.sai280';
 const HSTMAN_SAI280 = '/srv/data/hstman.sai280';
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/sai280/hstpl', function (req, res) {
 
   fs.readFile(HSTPL_SAI280, 'utf8', function(err, data) {
