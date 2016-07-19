a3925ad910a392105b300e4dfee8458931be1913
Pablo Santiago Semhan <santiagosemhan@gmail.com>
2016-07-19 08:44:29 -0300

    Se modifica el servidor node. Se agregan constantes con los nombres de los archivos. Se genera un evento a demanda del histórico de plumas.

--- node/server.js
+++ node/server.js
@@ -1,30 +1,37 @@
 var server      = require('http').createServer();
 var io          = require('socket.io')(server);
+var watch       = require('node-watch');
 
 const fs = require('fs');
 
+const TRPL_SAI280   = '/srv/data/trpl.sai280';
+const TRMAN_SAI280  = '/srv/data/trman.sai280';
+const HSTPL_SAI280  = '/srv/data/hstpl.sai280';
+const HSTMAN_SAI280 = '/srv/data/hstman.sai280';
+
+const WATCH_FILES = [TRPL_SAI280,TRMAN_SAI280,HSTPL_SAI280];
 
 // Funcion que parsea el csv y lo convierte a JSON
 function push($filename) {
 
 
-  fs.readFile($filename, function (err, data) {
+  fs.readFile($filename, "utf-8", function (err, data) {
 
-    if (err) throw err;
+    if (err) console.log(err);
 
     switch ($filename) {
 
-        case '/srv/data/trpl.sai280':
+        case TRPL_SAI280:
             // Emite un evento al socket del tipo csvOutput
             sai280.emit('trpl', data);
             break;
 
-        case '/srv/data/trman.sai280':
+        case TRMAN_SAI280:
             // Emite un evento al socket del tipo csvOutput
             sai280.emit('trman', data);
             break;
 
-        case '/srv/data/hstpl.sai280':
+        case HSTPL_SAI280:
             // Emite un evento al socket del tipo csvOutput
             sai280.emit('hstpl', data);
             break;
@@ -44,7 +51,8 @@ var sai280 = io.of('/sai280');
 
 // Detecta cuando alguien se conecta
 sai280.on('connection', function (socket) {
-    socket.on('event', function (data) {
+    socket.on('hstpl_demanda', function (data) {
+        push(HSTPL_SAI280);
     });
 
     socket.on('disconnect', function () {
@@ -54,7 +62,7 @@ sai280.on('connection', function (socket) {
 
 
 try{
-    watch(['/srv/data/trman.sai280', '/srv/data/trpl.sai280', '/srv/data/hstpl.sai280'], function(filename) {
+    watch(WATCH_FILES, function(filename) {
         //console.log(filename, ' changed.');
         push(filename);
     });
