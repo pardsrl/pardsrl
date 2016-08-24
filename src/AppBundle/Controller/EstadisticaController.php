749248c859951a4e83ce8e6d22fb16597c58b269
Pablo Santiago Semhan <santiagosemhan@gmail.com>
2016-08-24 14:02:53 -0300

    Fix commit anterior

--- src/AppBundle/Controller/EstadisticaController.php
+++ src/AppBundle/Controller/EstadisticaController.php
@@ -9,29 +9,58 @@ class EstadisticaController extends Controller
 {
     public function operacionesPorEquipoAction()
     {
+
+        $estadisticaManager = $this->get('manager.estadistica');
+
+        $operacionesPorEquipo = $estadisticaManager->getDistribucionOperacionesPorEquipo($this->getUser()->getPersona());
+
         return $this->render('AppBundle:estadistica:operaciones_por_equipo.html.twig', array(
-            // ...
+            'data' => json_encode($operacionesPorEquipo)
         ));
     }
 
     public function operacionesPorYacimientoAction()
     {
+        $estadisticaManager = $this->get('manager.estadistica');
+
+        $operacionesPorEquipo = $estadisticaManager->getDistribucionOperacionesPorYacimiento($this->getUser()->getPersona());
+
         return $this->render('AppBundle:estadistica:operaciones_por_yacimiento.html.twig', array(
-            // ...
+            'data' => json_encode($operacionesPorEquipo)
         ));
+
     }
 
     public function promediosCanosHoraAction()
     {
+        $estadisticaManager = $this->get('manager.estadistica');
+
+        $promediosCanosHora = $estadisticaManager->getPromediosCanoHora($this->getUser()->getPersona());
+
         return $this->render('AppBundle:estadistica:promedios_canos_hora.html.twig', array(
-            // ...
+            'data' => json_encode($promediosCanosHora)
         ));
     }
 
     public function factorTiempoUtilAction()
     {
+        $estadisticaManager = $this->get('manager.estadistica');
+
+        $factorTiempoUtil = $estadisticaManager->getFactorTiempoUtil($this->getUser()->getPersona());
+
         return $this->render('AppBundle:estadistica:factor_tiempo_util.html.twig', array(
-            // ...
+            'data' => json_encode($factorTiempoUtil)
+        ));
+    }
+
+    public function promediosVarillasHoraAction()
+    {
+        $estadisticaManager = $this->get('manager.estadistica');
+
+        $promediosVarillasHora = $estadisticaManager->getPromediosVarillasHora($this->getUser()->getPersona());
+
+        return $this->render('AppBundle:estadistica:promedios_varillas_hora.html.twig', array(
+            'data' => json_encode($promediosVarillasHora)
         ));
     }
 
